ccae556efe7d943f860f0d0f1c271cabb88a412d
karteek <karteekakasam@gmail.com>
2025-05-18 17:04:40 +0530

    first commit

---
+++ productservice/productservice/src/main/java/com/shoping/productservice/dto/Inventory.java
@@ -0,0 +1,16 @@
+package com.shoping.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+    private Long id;
+    private String productCode;
+    private Integer availableQuantity;
+}
